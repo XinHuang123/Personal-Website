41df940b97152e8800f59d03f5059d8ec072f536
XinHuang123 <Huang.xin1@husky.neu.edu>
2016-04-18 15:59:14 -0700

    Version 1.0

---
+++ public/js/scroll.js
@@ -0,0 +1,7 @@
+"use strict"
+jQuery(document).ready(function($) {
+    $(".scroll").click(function(event){		
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+    });
+});
---
+++ public/js/work_gallary.js
@@ -0,0 +1,10 @@
+"use strict";                   
+$(document).ready(function() {
+    $("#owl-demo3").owlCarousel({
+    items : 4,
+    lazyLoad : true,
+    autoPlay : true,
+    navigation: false,
+    pagination: false,
+    });
+});  
